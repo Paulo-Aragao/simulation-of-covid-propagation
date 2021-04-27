69052744fc0763d912b5cc026a61cec253c475ff
paulo-aragao <paulotorres12@gmail.com>
2021-04-27 03:37:37 -0300

    movement and infection functions added

--- simulation.py
+++ simulation.py
@@ -1,21 +1,32 @@
 from typing import List
-# import numpy as np
+from random import choices
+import numpy as np
+import math
 import csv
+from scipy.stats import bernoulli
+import random
 class Agent:
     '''Unit of the simulation'''
     def __init__(self, pos, home):
         self.center = None
         self.home = home
         self.pos = pos
+        self.speed = 5
+        self.situation = "infected"
 
 class Simulation:
     '''Standalone simulation'''
-    def __init__(self, filepath):
+    def __init__(self, filepath,infection_probability,proximity,recovery_probability,gravitation_tick):
         home_list, center_list = self._load_file()
         self._agents: List[Agent]  = self._create_agents(home_list)
         self._centers = self._create_centers(center_list)
         self._homes = home_list
-
+        agent = self._agents[0]
+        #hyperparametres
+        self._infection_probability = infection_probability
+        self._proximity = proximity
+        self._recovery_probability = recovery_probability
+        self._gravitation_tick = gravitation_tick
     def _load_file(self):
         file_path = 'output/points.csv'
         home_list = []
@@ -36,9 +47,19 @@ class Simulation:
     def _create_centers(self, center_list):
         return [center for center in center_list]
 
-    def get_agents(self):
-        return [agent.pos for agent in self._agents]
+    def _euclidian_distance(self, a, b):
+        return np.linalg.norm(np.array(a) - np.array(b))
 
+    def _choose_center(self, agent):
+        distances = [self._euclidian_distance(agent.pos, center) for center in self._centers]
+        weights =  [min(distances)/distance * 100 for distance in distances]
+        choosen = choices(distances, weights, k=1)
+        idx = distances.index(choosen)
+        return self._centers[idx]
+
+    def get_agents(self):
+        return self._agents
+    
     def get_centers(self):
         return self._centers
 
@@ -46,4 +67,38 @@ class Simulation:
         return self._homes
 
     def next_step(self):
-        pass
+        for agent in self._agents:
+            agent.center = self._choose_center(agent)
+            #recovery covid
+            if agent.situation == "infected":
+                if bernoulli.rvs(p=self._recovery_probability, size=1)[0] == 1:
+                    agent.situation = "safe"
+
+    def check_proximity_contamination(self,target_agent):
+        for agent in self._agents:
+            if agent.situation == "safe" and agent is not target_agent:
+                if self._euclidian_distance(target_agent.pos,agent.pos) < self._proximity:
+                    if bernoulli.rvs(p=self._infection_probability, size=1)[0] == 1:
+                        agent.situation = "infected"
+    def movement_agents(self):
+        for agent in self._agents:
+            #check proximity
+            if agent.situation == "infected":
+                self.check_proximity_contamination(agent)
+            angle = self.get_angle_between_points(agent.pos, agent.center)
+            agent.pos = self.move_coords_biased(agent.pos,angle,agent.speed)
+    def move_coords_biased(self,coords, angle, radius):
+        angle_biased = self.get_angle_biased(angle, 6)
+        x = radius * np.cos( angle + angle_biased )
+        y = radius * np.sin( angle + angle_biased )
+
+        new_coord = (coords[0] + x, coords[1] + y)
+        return new_coord
+
+    def get_angle_biased(self,a, k):
+        random_angle = np.random.uniform(-np.pi, np.pi)
+        angle = (1/(2*np.pi*np.i0(k)))*(np.e**(k*(a-random_angle)))
+        return angle
+
+    def get_angle_between_points(self,p1, p2):
+        return math.atan2(p2[1] - p1[1],p2[0] - p1[0])
--- simulation_pygame.py
+++ simulation_pygame.py
@@ -21,7 +21,7 @@ from simulation import Simulation
 class SimulationPygame:
     ''' Simulation '''
 
-    def __init__(self, surface: pygame.Surface):
+    def __init__(self, surface: pygame.Surface,infection_probability,proximity,recovery_probability,gravitation_tick):
         self._screen = surface
         self._widgets = self._build_widgets()
         self._uimanager = pygame_gui.UIManager(surface.get_size())
@@ -35,7 +35,7 @@ class SimulationPygame:
             home_poi.get_name(): home_poi,
             center_poi.get_name(): center_poi
         }
-        self._simulation = Simulation('output/points.csv')
+        self._simulation = Simulation('output/points.csv',infection_probability,proximity,recovery_probability,gravitation_tick)
 
     def _build_widgets(self):
         return []
@@ -43,8 +43,8 @@ class SimulationPygame:
     def _process_keys(self):
         keys = pygame.key.get_pressed()
         offset = {
-            pygame.K_w: (0,1), pygame.K_s: (0,-1),
-            pygame.K_a: (1,0), pygame.K_d: (-1,0)
+            pygame.K_w: (0,-1), pygame.K_s: (0,1),
+            pygame.K_a: (-1,0), pygame.K_d: (1,0)
         }
         speed_factor = (
             8 if keys[pygame.K_LCTRL] else 2 if keys[pygame.K_LSHIFT] else 4)
@@ -73,8 +73,11 @@ class SimulationPygame:
             screen.blit(self._available_points['home'].get_icon(), home + self._camera.offset)
         for agent in self._simulation.get_agents():
             agent_surface = pygame.Surface((5, 5))
-            agent_surface.fill((0, 255, 0))
-            screen.blit(agent_surface, agent + self._camera.offset)
+            if agent.situation == "safe":
+                agent_surface.fill((0, 255, 0))
+            else:
+                agent_surface.fill((255, 0, 0))
+            screen.blit(agent_surface, agent.pos + self._camera.offset)
         for widget in self._widgets:
             widget.draw()
 
@@ -86,10 +89,20 @@ pygame.init()
 screen = pygame.display.set_mode(SCREEN_SIZE)
 pygame.display.set_caption('Teste')
 clock = pygame.time.Clock()
-sim = SimulationPygame(screen)
+#hyperparamters of SARS-CoV-2
+infection_probability = 0.04
+proximity = 0.8
+recovery_probability = 0.1
+gravitation_tick = 30
+sim = SimulationPygame(screen,infection_probability,proximity,recovery_probability,gravitation_tick)
+
+tick_day = 30
+cooldown = 30
+time = 0
 #  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #  ~-~-~-~-~-~-~-~- MAIN LOOP ~-~-~-~-~-~-~-~-
 #  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+sim._simulation.next_step()
 while True:
     time_delta = clock.tick(30)/1000.0
     evts = pygame.event.get()
@@ -98,6 +111,13 @@ while True:
             pygame.quit()
     screen.fill((0,0,0))
     # <----- DRAW AND LISTEN ------>
+    #test moviment
+    time = time + 1
+    if time > cooldown:
+        cooldown = cooldown + tick_day
+        sim._simulation.next_step()
+    sim._simulation.movement_agents()
+    #
     sim.listen(evts, time_delta)
     sim.draw()
     # -----> DRAW AND LISTEN <-------
